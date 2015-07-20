93f5701a925f4c6da06ede50a8a53de5d4f4e896
gabemarshall <gabemarshall@me.com>
2015-07-20 12:05:24 -0400

    Improved error handling on payloads with callbacks

--- modules/questionUser.js
+++ modules/questionUser.js
@@ -8,23 +8,28 @@ var prompt = require('prompt'),
     checkModule = require('./checkModule.js'),
     finalAnswer
 
-if (checkModule.kexec()){
-     var kexec = require('kexec');
+if (checkModule.kexec()) {
+    var kexec = require('kexec');
 }
 
 exports.http = function(callback) {
     prompt.message = "Should I fire up a web server for you? (Y/n) :"
+
     prompt.get([{
         name: '_',
         description: ":"
     }], function(err, result) {
 
-        result._ = result._.toUpperCase()
-        if (result._ === "Y") {
-            var server = web.init();
-            callback(finalAnswer);
-        } else {
-            callback(finalAnswer);
+        try {
+            result._ = result._.toUpperCase();
+            if (result._ === "Y") {
+                var server = web.init();
+                callback(finalAnswer);
+            } else {
+                callback(finalAnswer);
+            }
+        } catch (err) {
+            console.log("\nLater bro!");
         }
     })
 }
@@ -36,26 +41,31 @@ exports.ncat = function(callback) {
         description: ":"
     }], function(err, result) {
 
-        result._ = result._.toUpperCase()
-        if (result._ === "Y") {
-            var port = db.getConfig("LPORT")
+        try {
+            result._ = result._.toUpperCase()
+            if (result._ === "Y") {
+                var port = db.getConfig("LPORT")
 
-            // kexec currently does not support windows
-            // if user is using windows, send them a nice error msg :/
-            if (!checkModule.kexec()){
-                console.log(log.red("\n[!] kexec module for netcat payloads appears to be missing!"));
-                console.log(log.blackBright("[*] To start a netcat listener, run the following => "+settings.netcat+" -lnp "+port+" -vv"));
-                callback(finalAnswer);
+                // kexec currently does not support windows
+                // if user is using windows, send them a nice error msg :/
+                if (!checkModule.kexec()) {
+                    console.log(log.red("\n[!] kexec module for netcat payloads appears to be missing!"));
+                    console.log(log.blackBright("[*] To start a netcat listener, run the following => " + settings.netcat + " -lnp " + port + " -vv"));
+                    callback(finalAnswer);
+                } else {
+                    console.log(log.blackBright("\n[*] Initializing hacking sequence (" + settings.netcat + " -lnp " + port + " -vv)"));
+                    callback(finalAnswer);
+                    kexec(settings.netcat + " -lnp " + port + " -vv");
+
+                }
+
             } else {
-                console.log(log.blackBright("\n[*] Initializing hacking sequence ("+settings.netcat+" -lnp "+port+" -vv)"));
                 callback(finalAnswer);
-                kexec(settings.netcat+" -lnp "+port+" -vv");
-
             }
-
-        } else {
-            callback(finalAnswer);
+        } catch (err) {
+            console.log("\nLater bro!");
         }
+
     })
 }
 
@@ -66,28 +76,32 @@ exports.ncatReceiveFile = function(callback) {
         description: ":"
     }], function(err, result) {
 
-        result._ = result._.toUpperCase()
-        if (result._ === "Y") {
-            var port = db.getConfig("LPORT");
-            var path = db.getConfig("PATH");
+        try {
+            result._ = result._.toUpperCase()
+            if (result._ === "Y") {
+                var port = db.getConfig("LPORT");
+                var path = db.getConfig("PATH");
 
-            if (!path || path.length <= 0){
-              log.yellow("Warning: Path variable is not set, defaulting to /var/tmp/")
-            }
-            path = "/var/tmp/"
-            // kexec currently does not support windows
-            // if user is using windows, send them a nice error msg :/
-            if (currentOS !== 'Darwin' && currentOS !== 'Linux'){
-                console.log("Sorry, currently this feature is unavailable in Windows. You'll have to manually start netcat: (Ex: netcat -lnp %s -vv", port);
+                if (!path || path.length <= 0) {
+                    log.yellow("Warning: Path variable is not set, defaulting to /var/tmp/")
+                }
+                path = "/var/tmp/"
+                    // kexec currently does not support windows
+                    // if user is using windows, send them a nice error msg :/
+                if (currentOS !== 'Darwin' && currentOS !== 'Linux') {
+                    console.log("Sorry, currently this feature is unavailable in Windows. You'll have to manually start netcat: (Ex: netcat -lnp %s -vv", port);
+                } else {
+                    callback(finalAnswer);
+                    var localFile = finalAnswer.replace(/(\/)/g, "_")
+                    console.log(log.blackBright("\n[*] Initializing hacking sequence (File will be saved as " + path + "/bros" + localFile + ")\n"))
+                    kexec(settings.netcat + " -lnp " + port + " > " + path + "/bros" + localFile + " -vv");
+                }
+
             } else {
                 callback(finalAnswer);
-                var localFile = finalAnswer.replace(/(\/)/g, "_")
-                console.log(log.blackBright("\n[*] Initializing hacking sequence (File will be saved as "+path+"/bros"+localFile+")\n"))
-                kexec(settings.netcat+" -lnp "+port+" > "+path+"/bros"+localFile+" -vv");
             }
-
-        } else {
-            callback(finalAnswer);
+        } catch (err) {
+            console.log("\nLater bro!");
         }
     })
 }
@@ -134,7 +148,7 @@ exports.some = function(question, callback, type) {
             } else {
                 clearInterval(checkStatus);
                 // If the last question is a string, send output to final parsing
-                if(typeof(question[temp - 1]) === "string"){
+                if (typeof(question[temp - 1]) === "string") {
                     callback(finalAnswer);
                 }
             }
