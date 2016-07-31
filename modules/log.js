9adbb0aee0f0a85fd42e0dfb2efb800d70c5da34
Gabe Marshall <gabemarshall@me.com>
2016-07-31 13:34:47 -0400

    Fixes for issue #12
    Optimized the blessed view for bros encode to better support Windows.

--- modules/log.js
+++ modules/log.js
@@ -10,4 +10,8 @@ color.okay = function(msg){
 	return color.green(msg);
 };
 
+color.normal = function(msg){
+  return color.white(msg);
+}
+
 module.exports = color;
--- modules/utilities.js
+++ modules/utilities.js
@@ -1,11 +1,21 @@
 
 var log = require('./log.js'),
-  	encoder = require('./encoder')
+    os = require('os');
 
 exports.update = function(){
   console.log("This feature is no longer available. To update Brosec run "+log.status("npm update -g Brosec")+" or do a "+log.status("git pull")+" from the Brosec directory");
 }
 
 exports.encoder = function(input){
+  var encoder = require('./encoder')
   encoder.init(input);
+}
+
+exports.isWindows = function(){
+  var currentOS = os.type();
+  var bool = false;
+  if (currentOS.match("Windows")){
+      bool = true;
+  }
+  return bool;
 }
